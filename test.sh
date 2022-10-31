3982bda37aeba1b9af146e67a3587d358a64dc07
flappeh <bonar_david@rocketmail.com>
2022-10-31 21:51:41 +0700

    updated bash script

---
+++ test.sh
@@ -0,0 +1,5 @@
+#test.sh
+echo "Test"
+read -p "Enter github commit message: " message
+echo $message
+echo "Finished."
--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #update.sh
 echo "Starting ReadTheDocs Update Script"
 read -p "Enter github commit message: " message
+echo "$message"
 make.exe html
 git add .
-git commit -m message
+git commit -m "$message"
 git push
 echo "Finished."
